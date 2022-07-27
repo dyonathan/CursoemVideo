707d4bd3176e1e8f9d38c94003b522baf23a1cdc
Dyonathan <dyonathanrodregues@hotmail.com>
2022-07-26 23:19:20 -0300

    atualização curso python

--- Mundo 2/PythonExercicios/ex070 compras.py
+++ Mundo 2/PythonExercicios/ex070 compras.py
@@ -1,6 +1,6 @@
 # Ler o nome e o preço de vários produtos e perguntar se quer continuar, no final
 # mostrar total gasto, quantos custam mais de 1000 e nome do mais barato
-
+'''
 nomebarato = 'J'
 continuar = 'J'
 totalgasto = 0
@@ -8,7 +8,7 @@ maisdemil = 0
 precobarato = 0
 
 print('-' * 30)
-print('     LOJA SUPER BARATÃO')
+print('{:-^30}'.format('LOJA SUPER BARATÃO'))
 print('-' * 30)
 
 while True:
@@ -41,3 +41,28 @@ print('----------- FIM DO PROGRAMA -------------')
 print(f'O total gasto nas compras foi R${totalgasto:.2f}!')
 print(f'{maisdemil} Produtos custaram mais de R$1000!')
 print(f'{nomebarato} e ele custou R${precobarato:.2f}!')
+'''
+
+# Guanabara fez
+
+total = totmil = menor = cont = 0
+batato = ''
+while True:
+    produto = str(input('Nome do produto: '))
+    preco = float(input('Preço: R$'))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format(' FIM DO PROGRAMA '))
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
--- Mundo 2/PythonExercicios/ex071 caixa eletronico.py
+++ Mundo 2/PythonExercicios/ex071 caixa eletronico.py
@@ -1,16 +1,14 @@
 # Perguntar o valor a sacar e mostrar
 # Quantas notas de 50, 20, 10 e 1 vao sair
 
+# Não consegui mostrar somente as notas que foram usadas!
+'''
 from math import floor
 
-n50 = 0
-n20 = 0
-n10 = 0
-n1 = 0
-resto = 0
+n50 = n20 = n10 = n1 = resto = 0
 
 print('=' * 30)
-print('          BANCO CEV')
+print('{:^30}'.format('BANCO CEV'))
 print('=' * 30)
 
 valor = float(input('Quanto você quer sacar? R$'))
@@ -37,6 +35,31 @@ if resto != 0:
 
 print('=' * 30)
 print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
+'''
+# Guanabara Fez
 
-
-
+print('=' * 30)
+print('{:^30}'.format('BANCO CEV'))
+print('=' * 30)
+valor = int(input('Que valor você quer sacar? R$'))
+total = valor
+ced = 50
+totced = 0
+while True:
+    if total >= ced:
+        total -= ced
+        totced += 1
+    else:
+        if totced > 0:
+            print(f'Total de {totced} células de R${ced}')
+        if ced == 50:
+            ced = 20
+        elif ced == 20:
+            ced = 10
+        elif ced == 10:
+            ced = 1
+        totced = 0
+        if total == 0:
+            break
+print('=' * 30)
+print('Volte sempre ao BANCO CEV! Tenha um bom dia!')
