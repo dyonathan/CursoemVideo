c8c6f5e8134b76a348f777e6135b2e8a47941b15
Dyonathan <dyonathanrodregues@hotmail.com>
2022-08-02 23:17:45 -0300

    atualização curso python

---
+++ Mundo 3/PythonExercicios/ex081 qtd decrescente e 5.py
@@ -0,0 +1,19 @@
+# Ler vários números mostrar, qtd, ordem decrescente e se tem o 5 na lista
+
+valores = list()
+
+
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    op = str(input('Quer Continuar? [S/N] ')).strip().lower()
+    if op in 'Nn':
+        break
+
+valores.sort(reverse=True)
+
+print(f'Foram lidos {len(valores)} números!')
+print(f'A ordem decrescente foi {valores}')
+if 5 in valores:
+    print(f'Avia o número 5 na lista!')
+else:
+    print('Não avia o número 5 na lista!')
