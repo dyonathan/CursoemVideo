3a9d6401cc3243c498627ebe5ffeab03143f0818
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-01 19:45:11 -0300

    atualização

--- Mundo 3/PythonExercicios/ex081 qtd decrescente e 5.py
+++ Mundo 3/PythonExercicios/ex081 qtd decrescente e 5.py
@@ -1,7 +1,6 @@
 # Ler vários números mostrar, qtd, ordem decrescente e se tem o 5 na lista
-
+'''
 valores = list()
-
 
 while True:
     valores.append(int(input('Digite um valor: ')))
@@ -18,3 +17,20 @@ if 5 in valores:
     print(f'Avia o número 5 na lista!')
 else:
     print('Não avia o número 5 na lista!')
+'''
+#Guanabara fez
+
+valores = []
+while True:
+    valores.append(int(input('Digite um valor: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+print('=-' * 30)
+print(f'Você digitou {len(valores)} elementos.')
+valores.sort(reverse=True)
+print(f'Os valores em ordem decrescente são {valores}')
+if 5 in valores:
+    print('O valor 5 faz parte da lista!')
+else:
+    print('O valor 5 não foi encontrado na lista!')
--- Mundo 3/PythonExercicios/ex082 lista normal, lista par e lista impar.py
+++ Mundo 3/PythonExercicios/ex082 lista normal, lista par e lista impar.py
@@ -1,5 +1,5 @@
 # Ler vários números em uma lista, criar mais duas uma com os números impares outro com os números pares
-
+'''
 valores = list()
 impar = list()
 par = list()
@@ -24,3 +24,23 @@ print('-=' * 26)
 print(f'A lista completa é {valores}')
 print(f'Os valores pares foram {par}')
 print(f'Os valores impares foram {impar}')
+'''
+#Guanabara fez
+
+num = list()
+pares = list()
+impares = list()
+while True:
+    num.append(int(input('Digite um número: ')))
+    resp = str(input('Quer continuar? [S/N] '))
+    if resp in 'Nn':
+        break
+for i, v in enumerate(num):
+    if v % 2 == 0:
+        pares.append(v)
+    else:
+        impares.append(v)
+print('-=' * 30)
+print(f'A lista completa é {num}')
+print(f'A lista de pares é {pares}')
+print(f'A lista de impares é {impares}')
