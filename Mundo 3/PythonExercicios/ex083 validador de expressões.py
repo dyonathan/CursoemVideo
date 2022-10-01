63b8e25971cee77b3ec61da9bbf7cf55776027cb
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-01 20:10:33 -0300

    atualização

--- Mundo 3/PythonExercicios/ex083 validador de expressões.py
+++ Mundo 3/PythonExercicios/ex083 validador de expressões.py
@@ -1,5 +1,6 @@
 # ler expressão que use parenteses, olhar se os parenteses estão corretos
-
+#Não consegui fazer
+'''
 expressao = list()
 
 asd = str(input('Fale uma expressão aritmetica: ')).strip().lower()
@@ -12,3 +13,23 @@ for c in range(0, len(sequencia)):
 
 print(sequencia)
 print(len(sequencia))
+'''
+
+#Guanabara fez
+
+expr = str(input('Digite a expressão: '))
+pilha = []
+for simb in expr:
+    if simb == '(':
+        pilha.append('(')
+    elif simb == ')':
+        if len(pilha) > 0:
+            pilha.pop()
+        else:
+            pilha.append(')')
+            break
+if len(pilha) == 0:
+    print('Sua expressão está valida!')
+else:
+    print('Sua expressão está errada!')
+
