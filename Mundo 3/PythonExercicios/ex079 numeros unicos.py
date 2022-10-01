9c3cd2586d774e4ed6f5cd396749b606043bc644
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-01 18:30:44 -0300

    atualização

--- Mundo 3/PythonExercicios/ex079 numeros unicos.py
+++ Mundo 3/PythonExercicios/ex079 numeros unicos.py
@@ -1,24 +1,39 @@
 # Ler n números, se for repetido pedir outro número, no final mostrar por ordem crescente
 
 valores = list()
-c = 0
 
 while True:
-    n = valores
+
     num = int(input('Digite um valor: '))
 
-    if num in n:
+    if num in valores:
         print('Valor duplicado! Não vou adicionar...')
     else:
         valores.append(int(num))
         print('Valor adicionado com sucesso...')
 
-    c += 1
     op = str(input('Quer digitar outro valor? [S/N] ')).strip()
     if op[0] in 'Nn':
         break
 
-
 valores.sort()
 print('-=' * 20)
 print(f'Você digitou os valores {valores}')
+'''
+# Guanabara fez
+
+numeros = list()
+while True:
+    n = int(input('Digite um valor: '))
+    if n not in numeros:
+        numeros.append(n)
+        print('Valor Adicionado com sucesso...')
+    else:
+        print('Valor duplicado! Não vou adicionar...')
+    r = str(input('Quer continuar? [S/N] '))
+    if r in 'Nn':
+        break
+print('-' * 30)
+numeros.sort()
+print(f'Você digitou os valores {numeros}')
+'''
--- Mundo 3/PythonExercicios/ex080 ordenar sem o sort.py
+++ Mundo 3/PythonExercicios/ex080 ordenar sem o sort.py
@@ -1,5 +1,5 @@
 # Digitar 5 valores colocar na lista já na ordem numérica sem usar o sort
-
+'''
 valores = list()
 reserva = list()
 
@@ -34,6 +34,23 @@ for c in range(0, 4):
 
 print('-=' * 26)
 print(f'Os valores digitados em ordem foram {valores}')
+'''
 
+# Guanabara fez
 
-
+lista = []
+for c in range(0, 5):
+    n = int(input('Digite um valor: '))
+    if c == 0 or n > lista[-1]:
+        lista.append(n)
+        print('Adicionando ao final da lista...')
+    else:
+        pos = 0
+        while pos < len(lista):
+            if n <= lista[pos]:
+                lista.insert(pos, n)
+                print(f'Adicionado na posição {pos} da lista...')
+                break
+            pos += 1
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram {lista}')
