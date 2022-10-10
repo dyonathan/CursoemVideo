4e7f50663f7f5f797283e474e264cc62068b94a3
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-09 23:14:32 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex088 palpite mega sena.py
+++ Mundo 3/PythonExercicios/ex088 palpite mega sena.py
@@ -1,4 +1,20 @@
 #Pergunta quantos jogos a pessoa quer fazer
 #Gerar altomaticamente os jogos (sortear 6 números de 1 a 60 em ordem crescente)
 
+from random import randint
+from time import sleep
 
+jogos = []
+qtd = []
+
+print('-=' * 15)
+print(f'{"MEGA SENA":^30}')
+print('-=' * 15)
+
+qtd = int(input(f'Quantos jogos serão feitos: '))
+for c in range(0, qtd):
+    jogos[c] = ' '
+
+
+print(qtd)
+print(jogos)
