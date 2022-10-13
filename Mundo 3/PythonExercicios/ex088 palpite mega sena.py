3234dc90c5fdf75ad1456edce198d580ad83030a
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-13 12:46:50 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex088 palpite mega sena.py
+++ Mundo 3/PythonExercicios/ex088 palpite mega sena.py
@@ -1,26 +1,56 @@
 #Pergunta quantos jogos a pessoa quer fazer
 #Gerar altomaticamente os jogos (sortear 6 números de 1 a 60 em ordem crescente)
-
+'''
 from random import randint
 from time import sleep
 
 jogos = []
-temp = []
 
 print('-=' * 15)
 print(f'{"MEGA SENA":^30}')
 print('-=' * 15)
 
 qtd = int(input(f'Quantos jogos serão feitos: '))
-for v in range(0, qtd):
-    print(f'O jogo é: ', end='')
-    for c in range(0, 6):
-        temp.append(randint(0, 60))
-        if jogos not in temp:
-            jogos.append(temp[:])
+print(f'-=-=-=-= SORTEANDO {qtd} JOGOS -=-=-=-=')
+for c in range(0, qtd):
+    while True:
+        jogo = randint(1, 60)
+        if jogo not in jogos:
+            jogos.append(jogo)
             if len(jogos) == 6:
-                jogos.sort()
-                print(jogos, end=' ')
-            temp.clear()
+                break
+    jogos.sort()
+    print(f'O jogo {c + 1} é: {jogos}')
+    sleep(1)
     jogos.clear()
-    print()
+print('-=-=-=-=-= < BOA SORTE > -=-=-=-=-=')
+'''
+#Guanabara fez
+
+from random import randint
+from time import sleep
+lista = list()
+jogos = list()
+print('-=' * 30)
+print('       JOGA NA MEGA SENA')
+print('-=' * 30)
+quant = int(input('Quantos jogos você quer que eu sorteie? '))
+tot = 1
+while tot <= quant:
+    cont = 0
+    while True:
+        num = randint(1, 60)
+        if num not in lista:
+            lista.append(num)
+            cont += 1
+        if cont >= 6:
+            break
+    lista.sort()
+    jogos.append(lista[:])
+    lista.clear()
+    tot += 1
+print('-=' * 3, f'SORTEANDO {quant} JOGOS ', '-=' * 3)
+for i, l in enumerate(jogos):
+    print(f'Jogo {i + 1}: {l}')
+    sleep(1)
+print('-=' * 5, '< BOA SORTE! >', '-=' * 5)
