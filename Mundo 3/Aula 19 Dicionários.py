a17cd23fa20fbc3f399c9720545342ed68f44526
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-18 00:24:43 -0300

    atualização curso python

--- Mundo 3/Aula 19 Dicionários.py
+++ Mundo 3/Aula 19 Dicionários.py
@@ -1,3 +1,5 @@
+#Começo da aula
+
 #Para tuplas usa-se () para Listas usa-se [] e parra dicionarios usa-se {}
 '''
 dados = list()#Cria uma lista com o nome dados
@@ -35,7 +37,7 @@ for k, v in filme.items():
 #1º mostra: O titulo é Star Wars
 #2º mostra: O ano é 1977
 #3º mostra: O diretor é George Lucas
-'''
+
 
 locadora = list()
 filme1 = {'titulo': 'Star Wars', 'ano': 1977, 'diretor': 'George Lucas'}
@@ -48,5 +50,69 @@ locadora.append(filme3)
 
 print(locadora[0]['ano'])#Mostra ano do campo 0 da locadora 1977
 print(locadora[2]['titulo'])#Mostra titulo do campo 2 da locadora Matrix
+
+
+#Pratica
+
+pessoa = {'nome': 'Gustavo', 'sexo': 'M', 'idade': 22}
+#print(f'O {pessoa["nome"]} tem {pessoa["idade"]} anos.')#Mostra 'O Gustavo tem 22 anos.'
+print(pessoa.keys())#Mostra nome, sexo e idade
+print(pessoa.values())#Mostra Gustavo, M e 22
+print(pessoa.items())#Mostra nome Gustavo, Sexo M e Idade 22
+
+for k in pessoa.keys():#Mostra as chaves, nome, sexo e idade
+    print(k)
+
+for k in pessoa.values():#Mostra os valores, Gustavo, M e 22
+    print(k)
+
+for k, v in pessoa.items():#Mostra os itens, nome = Gustavo, sexo = M e idade = 22
+    print(f'{k} = {v}')
+
+del pessoa['sexo']
+for k, v in pessoa.items():#Mostra os itens, nome = Gustavo, sexo = M e idade = 22
+    print(f'{k} = {v}')
+
+pessoa['nome'] = 'Leandro'
+for k, v in pessoa.items():#Troca o nome de Gustavo para Leandro
+    print(f'{k} = {v}')
+
+pessoa['peso'] = 98.5#Adiciona o campo peso e o valor 98.5 no dicionario
+for k, v in pessoa.items():
+    print(f'{k} = {v}')
+
+
+brasil = []
+estado1 = {'uf': 'Rio de Janeiro', 'sigla': 'RJ'}
+estado2 = {'uf': 'São Paulo', 'sigla': 'SP'}
+brasil.append(estado1)
+brasil.append(estado2)
+
+print(brasil[0]['uf'])#Mostra Rio de Janeiro
+print(brasil[1]['sigla'])#Mostra SP
+'''
+
+estado = dict()
+brasil = list()
+for c in range(0, 3):
+    estado['uf'] = str(input('Unidade Federativa: '))
+    estado['sigla'] = str(input('Sigla do Estado: '))
+    brasil.append(estado.copy())#Na lista para fazer copia usava o append(estado[:]) no dicionario usa o .copy()
+#print(brasil)#print normal, um na frente do outro
+
+#for e in brasil:
+#    print(e)#Mostra uf e sigla em cada linha
+
+#for e in brasil:
+#    for k, v in e.items():
+#        print(f'O campo {k} tem valor {v}')
+#vai imprimir o campo uf tem valor ...
+#o campo sigla tem valor ...
+#e vai repetir para todas as entradas
+
+for e in brasil:
+    for v in e.values():
+        print(v, end=' ') #Mostra a uf e a sigla juntos
+    print()
 
 
