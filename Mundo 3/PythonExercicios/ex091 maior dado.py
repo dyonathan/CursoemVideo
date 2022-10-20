c27a65fa4843980cb820fe6f90bc2a280f3c67a6
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-19 22:46:18 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex091 maior dado.py
+++ Mundo 3/PythonExercicios/ex091 maior dado.py
@@ -1,29 +1,19 @@
 #Ler 4 dados guardar em um dicionario, mostrar em ordem e falar qual o maior
 from random import randint
+from time import sleep
 
-jogadores = dict()
-dado = list()
-ordem = list()
+pos = 1
 
-'''
-for c in range(0, 4):
-    jogadores['dado'] = randint(1, 7)
-    dado.append(jogadores.copy())
+dado = [randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6)]
+jogadores = {'jogador1': dado[0], 'jogador2': dado[1], 'jogador3': dado[2], 'jogador4': dado[3]}
 
-ordem.append()
-#dado.sort()
-print(dado)
-'''
-j = list()
-d = list()
+print('Valores sorteados:')
+for k, v in jogadores.items():
+    print(f'   O {k} tirou {v}')
+    sleep(1)
 
-for c in range(1, 5):
-    j = randint(0, 7)
-    print(f'O jogador{c} tirou {j}')
-    d.append(j[:])
-
-d.sort()
-print('Ranking dos jogadores: ')
-for c, v in enumerate(d):
-    print(f'{c}º lugar: jogador{v} com')
-
+print('Ranking dos jogadores:')
+for k in sorted(jogadores, key=jogadores.get, reverse=True):
+    print(f'   {pos}º lugar: {k} com {jogadores[k]}')
+    sleep(1)
+    pos += 1
