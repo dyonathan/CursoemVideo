bf92c9bad2d93f7afe4644870e3b29c05663e862
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-25 11:35:47 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex091 maior dado.py
+++ Mundo 3/PythonExercicios/ex091 maior dado.py
@@ -1,11 +1,11 @@
 #Ler 4 dados guardar em um dicionario, mostrar em ordem e falar qual o maior
+'''
 from random import randint
 from time import sleep
 
 pos = 1
 
-dado = [randint(1, 6), randint(1, 6), randint(1, 6), randint(1, 6)]
-jogadores = {'jogador1': dado[0], 'jogador2': dado[1], 'jogador3': dado[2], 'jogador4': dado[3]}
+jogadores = {'jogador1': randint(1, 6), 'jogador2': randint(1, 6), 'jogador3': randint(1, 6), 'jogador4': randint(1, 6)}
 
 print('Valores sorteados:')
 for k, v in jogadores.items():
@@ -17,3 +17,25 @@ for k in sorted(jogadores, key=jogadores.get, reverse=True):
     print(f'   {pos}º lugar: {k} com {jogadores[k]}')
     sleep(1)
     pos += 1
+'''
+#Guanabara Fez
+
+from random import randint
+from time import sleep
+from operator import itemgetter
+jogo = {'jogador1': randint(1, 6),
+        'jogador2': randint(1, 6),
+        'jogador3': randint(1, 6),
+        'jogador4': randint(1, 6)}
+ranking = list()
+print('Valores sorteados: ')
+for k, v in jogo.items():
+    print(f'{k} tirou {v} no dado.')
+    sleep(1)
+ranking = sorted(jogo.items(), key=itemgetter(1), reverse=True)
+print('-=' * 30)
+print(' == RANKING DOS JOGADORES == ')
+for i, v in enumerate(ranking):
+    print(f'   {i + 1}º lugar: {v[0]} com {v[1]}.')
+    sleep(1)
+
