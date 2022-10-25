d02b2e66100754ad45d213a83bc2f3dd5bfac553
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-25 13:44:43 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex092 aposentadoria.py
+++ Mundo 3/PythonExercicios/ex092 aposentadoria.py
@@ -1,6 +1,7 @@
 #Ler o nome, ano de nascimento e carteira de trabalho, mas cadastrar a idade e não o nascimento
 #se CTPS diferente de zero cadastrar o ano de contratação e o salário
 #mostrar idade e ano de aposentadoria
+'''
 from datetime import date
 
 pessoa = {}
@@ -26,3 +27,21 @@ print('-=' * 30)
 print(pessoa)
 for k, v in pessoa.items():
     print(f'{k} tem o valor {v}')
+
+'''
+#Guanabara fez
+
+from datetime import datetime
+dados = dict()
+dados['nome'] = str(input('Nome: '))
+nasc = int(input('Ano de nascimento: '))
+dados['idade'] = datetime.now().year - nasc
+dados['ctps'] = int(input('Carteira de trabalho (0 não tem): '))
+if dados['ctps'] != 0:
+    dados['contratação'] = int(input('Ano de contratação: '))
+    dados['salário'] = float(input('Salário: R$'))
+    dados['aposentadoria'] = dados['idade'] + ((dados['contratação'] + 35) - datetime.now().year)
+print('-=' * 30)
+for k, v in dados.items():
+    print(f'  - {k} tem o valo {v}')
+
