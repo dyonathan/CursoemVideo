0831bb239302df5ca18f3cc6856d4de3a4fcd4aa
Dyonathan <dyonathanrodregues@hotmail.com>
2022-10-30 23:05:09 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex096 calcular área com função.py
+++ Mundo 3/PythonExercicios/ex096 calcular área com função.py
@@ -2,7 +2,7 @@
 #ler largura e compimento
 #mostrar área do terreno
 
-
+'''
 def area(largura, comprimento):
     a = largura * comprimento
     print(f'A área de um terreno {largura}x{comprimento} é de {a}m².')
@@ -11,3 +11,17 @@ def area(largura, comprimento):
 print('Controle de Terrenos')
 print('-' * 20)
 area(float(input('LARGURA (m): ')), float(input('COMPRIMENTO (m): ')))
+'''
+#Guanabara fez
+
+
+def area(larg, comp):
+    a = larg * comp
+    print(f'A área de um terreno {larg}x{comp} é de {a}m².')
+
+# Programa principal
+print(' Controle de Terrenos')
+print('-' * 20)
+l = float(input('LARGURA (m): '))
+c = float(input('COMPRIMENTO (m): '))
+area(l, c)
