6b0ffe44000971ae84d9da5da81db614da482a98
Dyonathan <dyonathanrodregues@hotmail.com>
2022-11-21 23:24:31 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex098 contador.py
+++ Mundo 3/PythonExercicios/ex098 contador.py
@@ -4,6 +4,7 @@
 #a)De 1 até 10, de 1 em 1
 #b)De 10 até 0, de 2 em 2
 #c)Uma contagem personalizada
+'''
 from time import sleep
 
 
@@ -53,3 +54,44 @@ def contador():
 
 
 contador()
+'''
+
+#Guanabara fez
+
+from time import sleep
+
+
+def contador(i, f, p):
+    if p < 0:
+        p *= -1
+    if p == 0:
+        p = 1
+    print('-=' * 20)
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    sleep(2.5)
+
+    if i < f:
+        cont = i
+        while cont <= f:
+            print(f'{cont} ', end='')
+            sleep(0.5)
+            cont += p
+        print('FIM!')
+    else:
+        cont = i
+        while cont >= f:
+            print(f'{cont} ', end='')
+            sleep(0.5)
+            cont -= p
+        print('FIM!')
+
+
+# Programa principal
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('-=' * 20)
+print('Agora é sua vez de personalizar a contagem!')
+ini = int(input('Inicio: '))
+fim = int(input('Fim:    '))
+pas = int(input('Passo:  '))
+contador(ini, fim, pas)
