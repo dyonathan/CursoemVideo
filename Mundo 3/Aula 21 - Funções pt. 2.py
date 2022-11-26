45a0a679f640863757cfe1b1fda91c21c7c0a17c
Dyonathan <dyonathanrodregues@hotmail.com>
2022-11-26 14:04:20 -0300

    atualização curso python

--- Mundo 3/Aula 21 - Funções pt. 2.py
+++ Mundo 3/Aula 21 - Funções pt. 2.py
@@ -87,5 +87,157 @@ print(f'No programa principal, n vale {x}')
 #O x funciona somente dentro de teste no programa principal ele da erro,
 #isso porque ele só existe dentro de teste isso é chamado de variavel local
 
+#Outro exemplo
 
+'''
+#Versão 1
+def teste(b):
+    #Escopo local
+    b += 4
+    c = 2
+    print(f'A dentro vale {a}')
+    print(f'B dentro vale {b}')
+    print(f'C dentro vale {c}')
+    #b e c só exitem dentro do teste, são variáveis locais
+    #b vale 9 e c vale 2 porque 5 do a global mais 4 do b local
+
+
+#Escopo global
+a = 5
+teste(a)
+print(f'A fora vale {a}')
+#Se colocar print b e c vai dar erro, porque eles só existem lá dentro do teste
+'''
+
+
+'''
+#Versão 2
+def teste(b):
+    #Escopo local
+    a = 8
+    b += 4
+    c = 2
+    print(f'A dentro vale {a}')
+    #Antes o a dentro valia 5 porque como não tinha a variável a local, ele usava o global, más
+    #como agora temos o a local o A dentro passa a valer 8 que é o valor local
+    print(f'B dentro vale {b}')
+    print(f'C dentro vale {c}')
+    #b e c só exitem dentro do teste, são variáveis locais
+    #b vale 9 e c vale 2 porque 5 do a global mais 4 do b local
+
+
+#Escopo global
+a = 5
+teste(a)
+print(f'A fora vale {a}')
+#Se colocar print b e c vai dar erro, porque eles só existem lá dentro do teste
+'''
+
+
+'''
+#Versão 3
+def teste(b):
+    #Escopo local
+    global a
+    #A função global a, faz com que o a local mude o a global, antes ele  valia 5 agora vale 8
+    a = 8
+    b += 4
+    c = 2
+    print(f'A dentro vale {a}')
+    print(f'B dentro vale {b}')
+    print(f'C dentro vale {c}')
+    #b e c só exitem dentro do teste, são variáveis locais
+    #b vale 9 e c vale 2 porque 5 do a global mais 4 do b local
+
+
+#Escopo global
+a = 5
+teste(a)
+print(f'A fora vale {a}')
+#Se colocar print b e c vai dar erro, porque eles só existem lá dentro do teste
+'''
+
+
+#Retorno de valores (return)
+
+
+'''
+def somar(a=0, b=0, c=0):
+    s = a + b + c
+    return s
+
+
+r1 = somar(3, 2, 5)
+r2 = somar(2, 2)
+r3 = somar(6)
+
+print(f'Os resultados foram {r1}, {r2} e {r3}.')
+'''
+
+
+#Aula pratica
+
+
+'''
+#Versão 1
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+#mostra somente o resultado de uma conta por vez 
+n = int(input('Digite um número: '))
+print(f'O fatorial de {n} é igual a {fatorial(n)}')
+'''
+
+
+'''
+#Versão 2
+def fatorial(num=1):
+    f = 1
+    for c in range(num, 0, -1):
+        f *= c
+    return f
+
+
+#Mostra o resultado de várias contas
+f1 = fatorial(5)
+f2 = fatorial(4)
+f3 = fatorial()
+print(f'Os resultados são {f1}, {f2} e {f3}')
+'''
+
+
+#A função return pode retornar outros tipos de dados alem de números
+
+
+'''
+#Versão 1
+def par(n=0):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+print(par(num))
+'''
+
+
+#Versão 2
+def par(n=0):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+
+
+num = int(input('Digite um número: '))
+if par(num):#Funcionou porque para definir o if usa True ou False por resultado de comparação aqui já foi direto
+    print('É par!')
+else:
+    print('Não é par!')
 
