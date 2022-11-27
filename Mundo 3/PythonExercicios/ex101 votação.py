0cb1d9870b1066e48a9c3a13f54640b28d2e7526
Dyonathan <dyonathanrodregues@hotmail.com>
2022-11-26 21:21:33 -0300

    atualização curso python

---
+++ Mundo 3/PythonExercicios/ex101 votação.py
@@ -0,0 +1,10 @@
+#Criar uma função chamada voto() que receba o ano de nascimento
+#Retornar Negado, Opcional ou Obrigatório
+from datetime import datetime
+
+#def votar(idade):
+
+
+nasc = int(input('Em que ano você nasceu? '))
+idade = datetime. - nasc
+print(idade)
