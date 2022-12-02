4cd423fdadea974de6517f8ab1c6a9eddee8bb77
Dyonathan <dyonathanrodregues@hotmail.com>
2022-12-02 20:48:24 -0300

    atualização curso python

--- Mundo 3/PythonExercicios/ex101 votação.py
+++ Mundo 3/PythonExercicios/ex101 votação.py
@@ -1,10 +1,40 @@
 #Criar uma função chamada voto() que receba o ano de nascimento
 #Retornar Negado, Opcional ou Obrigatório
-from datetime import datetime
+'''
+from datetime import date
 
-#def votar(idade):
+
+def voto(nasc):
+    print('-' * 30)
+    ano = date.today().year
+    idade = ano - nasc
+    if idade < 16:
+        votar = 'NÃO VOTA.'
+    elif idade >= 16 and idade < 18 or idade > 65:
+        votar = 'VOTO OPCIONAL.'
+    else:
+        votar = 'VOTO OBRIGATÓRIO.'
+    print(f'{idade} anos: {votar}')
 
 
+voto(nasc=int(input('Em que ano você nasceu? ')))
+'''
+
+
+#Guanabara fez
+
+
+def voto(ano):
+    from datetime import date
+    atual = date.today().year
+    idade = atual - ano
+    if idade < 18:
+        return f'Com {idade} anos: NÃO VOTA.'
+    elif 16 <= idade < 18 or idade > 65:
+        return f'Com {idade} anos: VOTO OPCIONAL.'
+    else:
+        return f'Com {idade} anos: VOTO OBRIGATORIO.'
+
+# Programa principal
 nasc = int(input('Em que ano você nasceu? '))
-idade = datetime. - nasc
-print(idade)
+print(voto(nasc))
